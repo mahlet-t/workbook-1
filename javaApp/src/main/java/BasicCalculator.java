d1d9e677abd3ceb0ee46b2c44486a739e4c65ede
mahle <mahletwondwossen71@gmail.com>
2025-04-11 11:57:04 -0500

    finished calculation

--- javaApp/src/main/java/BasicCalculator.java
+++ javaApp/src/main/java/BasicCalculator.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class BasicCalculator {
     public static void main(String[] args) {
-        Scanner input =new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.print("enter the first number");
-        double num1 =input.nextDouble();
+        double num1 = input.nextDouble();
         System.out.print("enter the second number:");
         double num2 = input.nextDouble();
 
@@ -17,14 +17,23 @@ public class BasicCalculator {
 
         System.out.println("please select an option: ");
 
-        String choice =input.next();
-        double result= num1 * num2;
-        System.out.println((int)num1 +" * " + (int)num2 + " = "+(int)result );
+        String choice = input.next();
+        if (choice.equals("M")) {
+            double result = num1 * num2;
+            System.out.println((int) num1 + " * " + (int) num2 + " = " + (int) result);
+        } else if (choice.equals("A")) {
+            double result = num1 + num2;
+            System.out.println((int) num1 + "+" + (int) num2 + "=" + (int) result);
+        } else if (choice.equals("s")) {
+            double result = num1 - num2;
+            System.out.println((int) num1 + "-" + (int) num2 + "=" + (int) result);
+        } else if (choice.equals("d")) {
+            double result = num1 / num2;
+            System.out.println((int) num1 + "/" + (int) num2 + "=" + result);
+
+        } else {
+            System.out.println("you chose other operation try again");
 
 
-
-
-
-
-    }
-}
+        }
+    } }
