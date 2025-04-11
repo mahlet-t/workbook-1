9944a8c5ea006a64ace8c5f78f7d788ea6fae3ce
mahle <mahletwondwossen71@gmail.com>
2025-04-10 21:31:30 -0500

    completed Mod3-exercise 3 - basic calculator

---
+++ javaApp/src/main/java/BasicCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.print("enter the first number");
+        float num1 =input.nextFloat();
+        System.out.print("enter the second number:");
+        float num2 = input.nextFloat();
+        System.out.println("please select an operation:");
+        System.out.println("A=add");
+        System.out.println("S=subtract");
+        System.out.println("M= multiply");
+        System.out.println("D= divide");
+        String choice =input.next();
+
+
+
+
+
+    }
+}
