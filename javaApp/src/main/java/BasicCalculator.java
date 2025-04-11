60d0ffca2b05a27a05986fdb3e6516309da2b9c8
mahle <mahletwondwossen71@gmail.com>
2025-04-11 11:01:39 -0500

    add calculation

--- javaApp/src/main/java/BasicCalculator.java
+++ javaApp/src/main/java/BasicCalculator.java
@@ -3,16 +3,24 @@ import java.util.Scanner;
 public class BasicCalculator {
     public static void main(String[] args) {
         Scanner input =new Scanner(System.in);
+
         System.out.print("enter the first number");
-        float num1 =input.nextFloat();
+        double num1 =input.nextDouble();
         System.out.print("enter the second number:");
-        float num2 = input.nextFloat();
-        System.out.println("please select an operation:");
+        double num2 = input.nextDouble();
+
+        System.out.println("possible calculation:");
         System.out.println("A=add");
         System.out.println("S=subtract");
         System.out.println("M= multiply");
         System.out.println("D= divide");
+
+        System.out.println("please select an option: ");
+
         String choice =input.next();
+        double result= num1 * num2;
+        System.out.println((int)num1 +" * " + (int)num2 + " = "+(int)result );
+
 
 
 
